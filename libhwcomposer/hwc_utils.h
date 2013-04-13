81f7a9e06d1c07d4ef077441301c3ef706355453
Saurabh Shah <saurshah@codeaurora.org>
2013-04-12 17:09:00 -0700

    hwc/overlay: Fix dma mode design and issues.
    Add support for tracking the mode in which DMA is being used in overlay.
    Remove unnecessary tracking variables, and their set,reset inconsistency.
    
    With such varibles in hwc, it still leads to overlay giving out DMA pipes.
    Make overlay as the single tracking place.
    
    Change-Id: Ib949f3d0ef5918ef323c81cb1768b68a68c83da4

--- libhwcomposer/hwc_utils.h
+++ libhwcomposer/hwc_utils.h
@@ -304,10 +304,6 @@ struct hwc_context_t {
     mutable Locker mExtLock;
     //Vsync
     struct vsync_state vstate;
-    //DMA used for rotator
-    bool mDMAInUse;
-    //MDP rotater needed
-    bool mNeedsRotator;
     //Check if base pipe is set up
     bool mBasePipeSetup;
 };
